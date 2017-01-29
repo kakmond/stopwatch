49e254154c6e8ed47d454ea5994f25012f8ca29e
kakmond <wongsathorn.pa@ku.th>
2017-01-29 22:33:42 +0700

    Update Main.java

--- src/stopwatch/Main.java
+++ src/stopwatch/Main.java
@@ -9,9 +9,18 @@ package stopwatch;
 
 public class Main {
 	public static void main(String[] args) {
-		Runnable[] tasks = { new AppendStringTask(50000), new AppendStringTask(100000),
-				new AppendStringBuilderTask(100000), new SumDoublePrimitiveTask(1000000000),
-				new SumDoubleTask(1000000000), new SumBigDecimalTask(1000000000) };
+		
+		/*
+		taskTimer.measureAndPrint(new AppendToString(50000));
+		taskTimer.measureAndPrint(new AppendToString(100000));
+		taskTimer.measureAndPrint(new AppendToStringBuilder(100000));
+		taskTimer.measureAndPrint(new SumDoublePrimitive(1000000000));
+		taskTimer.measureAndPrint(new SumDouble(1000000000));
+		taskTimer.measureAndPrint(new SumBigDecimal(1000000000));
+		*/
+		
+	Runnable[] tasks = { new AppendStringTask(50000), new AppendStringTask(100000), new AppendStringBuilderTask(100000),
+			new SumDoublePrimitiveTask(1000000000), new SumDoubleTask(1000000000), new SumBigDecimalTask(1000000000) };
 
 		for (Runnable task : tasks) {
 			TaskTimer.measureAndPrint(task);
